bcec1474502ecf544872b966a9fd46cc41c25e1c
Metallifax <mathew_forgione@msn.com>
2022-02-15 05:43:52 -0400

    added more tests to fibonacci to test to ci/cd

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,24 @@
-# testing Fibonacci number function
+# fibonacci function
 def fib(n: int) -> int:
     return n if n < 2 else fib(n - 1) + fib(n - 2)
 
 
-def test_fibonacci():
+# tests
+def test_fib_of_negative_one():
+    assert fib(-1) == -1
+
+
+def test_fib_of_zero():
+    assert fib(0) == 0
+
+
+def test_fib_of_one():
+    assert fib(1) == 1
+
+
+def test_fib_of_10():
     assert fib(10) == 55
+
+
+def test_fib_of_twenty():
+    assert fib(20) == 6765
